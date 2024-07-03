bd3ac4d30ffbdb17a22a7d22ebd2f8e9989379dd
Suchark <Suchanuch28@gmail.com>
2024-07-03 19:15:45 +0700

    update index

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,44 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -22,38 +50,40 @@ const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 if (!port) {
-    console.error("PORT environment variable is not defined.");
-    process.exit(1);
+  console.error("PORT environment variable is not defined.");
+  process.exit(1);
 }
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Our Server");
+  res.send("Our Server");
 });
-app.get("/getUser", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/getUser", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const User = yield prisma.user.findMany();
-        res.json(User);
+      const User = yield prisma.user.findMany();
+      res.json(User);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: "Internal Server Error", details: error });
     }
-    catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(500).json({ error: "Internal Server Error", details: error });
-    }
-}));
-app.get("/getDevice", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+app.get("/getDevice", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const Device = yield prisma.device.findMany();
-        res.json(Device);
+      const Device = yield prisma.device.findMany();
+      res.json(Device);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: "Internal Server Error", details: error });
     }
-    catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(500).json({ error: "Internal Server Error", details: error });
-    }
-}));
+  })
+);
 app.use((err, req, res, next) => {
-    console.error("Unhandled error:", err);
-    res.status(500).json({ error: "Something went wrong!", details: err });
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: "Something went wrong!", details: err });
 });
 app.listen(parseInt(port), () => {
-    console.log(`Backend started at port: ${port}.`);
+  console.log(`Backend started at port: ${port}.`);
 });
