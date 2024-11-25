5b47a6c7ee93416767fff1185d4264aea3ac9c62
Smart309 <pattharapon_takham@cmu.ac.th>
2024-11-25 12:54:58 +0700

    "Add function start()"

--- src/App.ts
+++ src/App.ts
@@ -7,28 +7,48 @@ import User from "./routes/Userapi";
 import Interface from "./routes/Interfaceapi";
 import Alert from "./routes/Alertapi";
 import deviceRoutes from "./routes/Deviceapi";
+import Host from "./routes/Host";
+// import Template from "./routes/Template";
+// import Item from "./routes/Item";
+// import History from "./routes/History";
 // import Details from "./routes/Details";
-
-dotenv.config();
 
 const app: Express = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+async function start() {
+  try {
+    dotenv.config();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Our Server");
-});
+    app.use(cors());
+    app.use(bodyParser.json());
 
-app.use("/getUser", User);
-app.use("/getDevice", deviceRoutes);
-app.use("/getInterface", Interface);
-app.use("/getAlert", Alert);
-// app.use("/getDetails", Details);
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      console.log(`${req.method} ${req.url}`);
+      next();
+    });
+    app.get("/", (req: Request, res: Response) => {
+      res.send("Our Server");
+    });
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error("Unhandled error:", err);
-  res.status(500).json({ error: "Something went wrong!" });
-});
+    app.use("/getUser", User);
+    app.use("/getDevice", deviceRoutes);
+    app.use("/getInterface", Interface);
+    app.use("/getAlert", Alert);
+    // app.use("/getDetails", Details);
+    app.use("/host", Host);
+    // app.use("/template", Template);
+    // app.use("/item", Item);
+    // app.use("/history", History);
+
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error("Unhandled error:", err);
+      res.status(500).json({ error: "Something went wrong!" });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+start();
 
 export default app;
