a1d776c93b9ff1ce0a22a9ac3bc1a74d57694eb9
Smart309 <pattharapon_takham@cmu.ac.th>
2024-11-26 15:10:40 +0700

    "Remodel"

--- src/App.ts
+++ src/App.ts
@@ -1,20 +1,11 @@
 // src/app.ts
 import express, { Express, Request, Response, NextFunction } from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import User from "./routes/Userapi";
-import Interface from "./routes/Interfaceapi";
-import Alert from "./routes/Alertapi";
-import deviceRoutes from "./routes/Deviceapi";
-import Host from "./routes/Host";
-import Template from "./routes/Template";
-import Item from "./routes/Item";
-import History from "./routes/History";
-// import Details from "./routes/Details";
-import { connectDb } from "./services/database";
 import { fetchAndStoreSnmpData } from "./services/snmpService";
-
+import { connectDb } from "./services/database";
+import bodyParser from "body-parser";
+import { routes } from "./apis";
+import dotenv from "dotenv";
+import cors from "cors";
 const app: Express = express();
 
 async function start() {
@@ -35,19 +26,8 @@ async function start() {
       console.log(`${req.method} ${req.url}`);
       next();
     });
-    app.get("/", (req: Request, res: Response) => {
-      res.send("Our Server");
-    });
 
-    app.use("/getUser", User);
-    app.use("/getDevice", deviceRoutes);
-    app.use("/getInterface", Interface);
-    app.use("/getAlert", Alert);
-    // app.use("/getDetails", Details);
-    app.use("/host", Host);
-    app.use("/template", Template);
-    app.use("/item", Item);
-    app.use("/history", History);
+    app.use("/", routes);
 
     setInterval(async () => {
       try {
@@ -58,16 +38,9 @@ async function start() {
         console.error("Error in scheduled SNMP data fetching:", error);
       }
     }, 10000);
-
-    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      console.error("Unhandled error:", err);
-      res.status(500).json({ error: "Something went wrong!" });
-    });
   } catch (err) {
     console.error(err);
   }
 }
 
 start();
-
-export default app;
---
+++ src/apis/index.ts
@@ -0,0 +1,23 @@
+// src/routes.ts
+import express from "express";
+import User from "./Userapi";
+import Interface from "./Interfaceapi";
+import Alert from "./Alertapi";
+import deviceRoutes from "./Deviceapi";
+import Host from "./Host";
+import Template from "./Template";
+import Item from "./Item";
+import History from "./History";
+// import Details from "./Details";
+
+export const routes = express.Router();
+
+routes.use("/getUser", User);
+routes.use("/getDevice", deviceRoutes);
+routes.use("/getInterface", Interface);
+routes.use("/getAlert", Alert);
+// routes.use("/getDetails", Details);
+routes.use("/host", Host);
+routes.use("/template", Template);
+routes.use("/item", Item);
+routes.use("/history", History);
