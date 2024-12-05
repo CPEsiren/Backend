b0fe80d16a770d282f4506cca00ffb360786c198
Smart309 <pattharapon_takham@cmu.ac.th>
2024-12-05 09:35:38 +0700

    Correct the typo OIT to OID.

--- src/models/Template.ts
+++ src/models/Template.ts
@@ -1,11 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 const templateSchema = new mongoose.Schema({
+  template_id: { type: mongoose.Schema.Types.ObjectId, unique: true },
   name_template: { type: String, required: true, unique: true },
   items: [
     {
       name_item: { type: String },
-      oit: { type: String },
+      oid: { type: String },
       type: { type: String },
       unit: { type: String },
     },
