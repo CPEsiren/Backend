49ced24427b76cb785df8df8ed4f222418af13ec
Smart309 <pattharapon_takham@cmu.ac.th>
2025-01-20 12:04:01 +0700

    Add route for fetching data between specified dates in Data API

--- src/apis/Data_api.ts
+++ src/apis/Data_api.ts
@@ -1,15 +1,11 @@
-// import { fetchAndStoreSnmpData } from "../services/snmpService";
-import { body, validationResult } from "express-validator";
-import { Router, Request, Response } from "express";
-import { Db, MongoClient, ObjectId } from "mongodb";
-import dotenv from "dotenv";
-import { getAllData, getData } from "../controllers/dataController";
+import { Router } from "express";
+import { getAllData, getBetween, getData } from "../controllers/dataController";
 
 const router = Router();
 
-dotenv.config();
-
 router.get("/", getAllData);
+
+router.get("/between", getBetween);
 
 router.get("/:id", getData);
 
