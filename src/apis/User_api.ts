53793413bb6e2849d842de418f8b764ff9ce69fe
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-03 12:31:50 +0700

    Add CRUD operations for user management in User API

--- src/apis/User_api.ts
+++ src/apis/User_api.ts
@@ -10,9 +10,13 @@ import {
 const router = express.Router();
 
 router.get("/", getUsers);
+
 router.get("/:id", getUser);
+
 router.post("/", createUser);
+
 router.put("/:id", updateUser);
+
 router.delete("/:id", deleteUser);
 
 export default router;
