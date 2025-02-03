74d4ecd7d00c4c62a6a7b61397cbd71472aa53e3
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-03 18:12:44 +0700

    Remove create and update user routes from User API

--- src/apis/User_api.ts
+++ src/apis/User_api.ts
@@ -1,21 +1,11 @@
 import express from "express";
-import {
-  getUser,
-  getUsers,
-  createUser,
-  updateUser,
-  deleteUser,
-} from "../controllers/userController";
+import { getUser, getUsers, deleteUser } from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
 router.get("/:id", getUser);
-
-router.post("/", createUser);
-
-router.put("/:id", updateUser);
 
 router.delete("/:id", deleteUser);
 
