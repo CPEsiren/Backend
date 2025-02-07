422ae7a2420eb4e5ffdfde1d6f48db429a3d06b6
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-07 21:28:21 +0700

    Remove logging middleware calls from triggerController for cleaner response handling

--- src/controllers/triggerController.ts
+++ src/controllers/triggerController.ts
@@ -1,6 +1,12 @@
-import { addLog } from "../middleware/log";
 import { Request, Response } from "express";
 import Trigger from "../models/Trigger";
+import Item from "../models/Item";
+import mongoose from "mongoose";
+import {
+  parseExpressionDetailed,
+  parseExpressionToItems,
+} from "../services/parserService";
+import Host from "../models/Host";
 
 const getTrigger = async (req: Request, res: Response) => {
   try {
@@ -8,34 +14,44 @@ const getTrigger = async (req: Request, res: Response) => {
 
     const trigger = await Trigger.findById(id);
     if (!trigger) {
-      await addLog("WARNING", "Trigger not found", false);
       return res.status(404).json({ message: "Trigger not found" });
     }
 
-    await addLog("INFO", "Trigger retrieved successfully", false);
     res.status(200).json(trigger);
   } catch (error) {
-    await addLog("ERROR", `Error retrieving trigger: ${error}`, false);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 const getTriggers = async (req: Request, res: Response) => {
   try {
-    const { severity, enabled } = req.query;
+    const data = await Trigger.aggregate([
+      {
+        $group: {
+          _id: "$host_id",
+          triggers: { $push: "$$ROOT" },
+        },
+      },
+      {
+        $project: {
+          host_id: "$_id",
+          triggers: 1,
+          _id: 0,
+        },
+      },
+    ]);
 
-    const query: any = {};
-    if (severity) query.severity = severity;
-    if (enabled !== undefined) query.enabled = enabled === "true";
+    await Host.populate(data, {
+      path: "host_id",
+      model: "Host",
+      select: "hostname",
+    });
 
-    const triggers = await Trigger.find(query);
-
-    await addLog("INFO", "Triggers retrieved successfully", false);
     res.status(200).json({
-      triggers,
+      status: "success",
+      data,
     });
   } catch (error) {
-    await addLog("ERROR", `Error retrieving triggers: ${error}`, false);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -45,63 +61,97 @@ const createTrigger = async (req: Request, res: Response) => {
     const {
       trigger_name,
       host_id,
-      item_id,
-      ComparisonOperator,
-      valuetrigger,
       severity,
+      expression,
+      ok_event_generation,
+      recovery_expression,
+      enabled,
     } = req.body;
 
+    const requiredFields = [
+      "trigger_name",
+      "host_id",
+      "severity",
+      "expression",
+      "ok_event_generation",
+      "enabled",
+    ];
+
+    const missingFields = requiredFields.filter((field) => !req.body[field]);
+
     // Validate input
-    if (
-      !trigger_name ||
-      !host_id ||
-      !item_id ||
-      !ComparisonOperator ||
-      valuetrigger === undefined ||
-      !severity
-    ) {
-      await addLog("WARNING", "Missing required fields", false);
-      return res.status(400).json({ message: "Missing required fields" });
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Missing required fields.",
+        requiredFields: missingFields,
+      });
     }
 
-    // Check if a trigger with the same name already exists
-    const existingTrigger = await Trigger.findOne({ trigger_name, severity });
-    if (existingTrigger) {
-      await addLog(
-        "WARNING",
-        "Trigger with this name and severity already exists",
-        false
-      );
-      return res.status(409).json({
-        message: "Trigger with this name and severity already exists",
+    const items: [string, mongoose.Types.ObjectId][] = [];
+    const addedItemNames = new Set<string>();
+
+    //parse expression
+    const parsedItemsExp = new Set(parseExpressionToItems(expression));
+    const logicExpression = parseExpressionDetailed(expression).map((item) => {
+      if (Array.isArray(item) && item.length === 3) {
+        return "false"; // แทนที่เงื่อนไขด้วย 'false'
+      }
+      return item[0].toLowerCase(); // คงค่า 'or' หรือ 'and' ไว้
+    });
+    if (recovery_expression) {
+      const parsedRecoveryItems = parseExpressionToItems(recovery_expression);
+      parsedRecoveryItems.forEach((item) => parsedItemsExp.add(item));
+    }
+
+    if (parsedItemsExp.size === 0) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid expression format",
       });
+    }
+
+    const logicRecoveryExpression = parseExpressionDetailed(
+      recovery_expression
+    ).map((item) => {
+      if (Array.isArray(item) && item.length === 3) {
+        return "false"; // แทนที่เงื่อนไขด้วย 'false'
+      }
+      return item[0].toLowerCase(); // คงค่า 'or' หรือ 'and' ไว้
+    });
+
+    for (const itemName of parsedItemsExp) {
+      if (!addedItemNames.has(itemName)) {
+        const item = await Item.findOne({ item_name: itemName });
+        if (item) {
+          items.push([itemName, item._id]);
+          addedItemNames.add(itemName);
+        }
+      }
     }
 
     // Create new trigger
     const newTrigger = new Trigger({
       trigger_name,
       host_id,
-      item_id,
-      ComparisonOperator,
-      valuetrigger,
       severity,
-      enabled: true, // Default to enabled
+      expression,
+      logicExpression,
+      items,
+      ok_event_generation,
+      recovery_expression,
+      logicRecoveryExpression,
+      enabled,
     });
 
     // Save the trigger
     await newTrigger.save();
 
-    await addLog(
-      "INFO",
-      `Trigger [${newTrigger.trigger_name}] created successfully`,
-      true
-    );
     res.status(201).json({
       message: `Trigger [${newTrigger.trigger_name}] created successfully`,
       trigger: newTrigger,
     });
   } catch (error) {
-    await addLog("ERROR", `Error creating trigger: ${error}`, false);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -110,66 +160,33 @@ const updateTrigger = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const {
-      name,
-      host_id,
-      item_id,
-      ComparisonOperator,
-      valuetrigger,
+      trigger_name,
       severity,
+      expression,
+      recovery_expression,
+      ok_event_generation,
       enabled,
     } = req.body;
 
-    // Find the trigger
-    const trigger = await Trigger.findById(id);
-    if (!trigger) {
-      await addLog("WARNING", "Trigger not found", false);
-      return res.status(404).json({ message: "Trigger not found" });
-    }
+    const trigger = await Trigger.findByIdAndUpdate(id, {
+      trigger_name,
+      severity,
+      expression,
+      recovery_expression,
+      ok_event_generation,
+      enabled,
+    });
 
-    // Update fields if they are provided
-    if (name !== undefined) {
-      // Check if the new name already exists (excluding the current trigger)
-      const existingTrigger = await Trigger.findOne({ name, _id: { $ne: id } });
-      if (existingTrigger) {
-        await addLog("WARNING", "Trigger with this name already exists", false);
-        return res
-          .status(409)
-          .json({ message: "Trigger with this name already exists" });
-      }
-      trigger.trigger_name = name;
-    }
-    if (host_id !== undefined) trigger.host_id = host_id;
-    if (item_id !== undefined) trigger.item_id = item_id;
-    if (ComparisonOperator !== undefined)
-      trigger.ComparisonOperator = ComparisonOperator;
-    if (valuetrigger !== undefined) trigger.valuetrigger = valuetrigger;
-    if (severity !== undefined) trigger.severity = severity;
-    if (enabled !== undefined) trigger.enabled = enabled;
-
-    // Validate ComparisonOperator if it's being updated
-    if (ComparisonOperator !== undefined) {
-      const validOperators = ["<", "<=", "=", ">=", ">", "!="];
-      if (!validOperators.includes(ComparisonOperator)) {
-        await addLog("WARNING", "Invalid ComparisonOperator", false);
-        return res.status(400).json({ message: "Invalid ComparisonOperator" });
-      }
-    }
-
-    // Save the updated trigger
-    await trigger.save();
-
-    await addLog(
-      "INFO",
-      `Trigger [${trigger.trigger_name}] updated successfully`,
-      true
-    );
     res.status(200).json({
-      message: `Trigger [${trigger.trigger_name}] updated successfully`,
+      status: "success",
+      message: `Trigger [${id}] updated successfully`,
       trigger,
     });
   } catch (error) {
-    await addLog("ERROR", `Error updating trigger: ${error}`, false);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({
+      status: "fail",
+      message: "Internal server error",
+    });
   }
 };
 
@@ -181,18 +198,11 @@ const deleteTrigger = async (req: Request, res: Response) => {
     const trigger = await Trigger.findByIdAndDelete(id);
 
     if (!trigger) {
-      await addLog("WARNING", "Trigger not found", false);
       return res.status(404).json({ message: "Trigger not found" });
     }
 
-    await addLog(
-      "INFO",
-      `Trigger [${trigger.trigger_name}] deleted successfully`,
-      true
-    );
     res.status(200).json({ message: "Trigger deleted successfully" });
   } catch (error) {
-    await addLog("ERROR", `Error deleting trigger: ${error}`, false);
     res.status(500).json({ message: "Internal server error" });
   }
 };
