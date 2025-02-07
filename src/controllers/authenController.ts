9d52dfee28fa2bfaf3ee0abd7b355097da8781dc
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-07 21:27:10 +0700

    Refactor loginUser to improve token validation and user creation logic

--- src/controllers/authenController.ts
+++ src/controllers/authenController.ts
@@ -1,51 +1,38 @@
 import { Request, Response } from "express";
 import { verifyToken } from "../services/authenService";
-import { User } from "../models/User";
-import { addLog } from "../middleware/log";
+import { IUser, User } from "../models/User";
 
 export const loginUser = async (req: Request, res: Response) => {
-  try {
-    const { token } = req.body;
+  const { token } = req.body;
+  let user: IUser | null;
 
+  try {
     const userInfo = await verifyToken(token);
 
-    if (!userInfo?.email_verified) {
-      return res.status(400).json({ error: "Email not verified" });
+    if (!userInfo || !userInfo.email_verified) {
+      return res.status(400).json({ error: "Invalid token" });
     }
 
-    const user = await User.findOne({ email: userInfo.email });
+    user = await User.findOne({ email: userInfo.email });
+
     if (!user) {
-      const newUser = new User({
+      user = new User({
         username: userInfo.name,
         email: userInfo.email,
         picture: userInfo.picture,
-        token: token,
-      });
-      const resUser = await newUser.save();
-      await addLog(
-        "INFO",
-        `User [${resUser.username}]created and login successfully`,
-        true
-      );
-      res.status(201).json({
-        message: `User [${resUser.username}]created and login successfully`,
-        user: resUser,
+        token,
       });
     } else {
       user.token = token;
-      const resUser = await user.save();
-      await addLog(
-        "INFO",
-        `User [${resUser.username}] created and login successfully`,
-        true
-      );
-      res.status(201).json({
-        message: `User [${resUser.username}] login successfully`,
-        user: resUser,
-      });
     }
+
+    const savedUser = await user.save();
+
+    res.status(201).json({
+      message: `User [${savedUser.username}] logged in successfully`,
+      user: savedUser,
+    });
   } catch (error) {
-    await addLog("WARNING", `Invalid token: ${error}`, false);
     res.status(400).json({ error: "Invalid token" });
   }
 };
