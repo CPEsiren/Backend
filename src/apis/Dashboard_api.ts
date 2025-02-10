30fcd56acfbb40b29e10ff68e5b6b246e3de511d
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-10 11:57:31 +0700

    Add CRUD operations for dashboards in Dashboard API

--- src/apis/Dashboard_api.ts
+++ src/apis/Dashboard_api.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { getDashboardCounts } from "../controllers/dashboardController";
+import {
+  createDashboard,
+  deleteDashboard,
+  getDashboardCounts,
+  getDashboards,
+  updateDashboard,
+} from "../controllers/dashboardController";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/count", getDashboardCounts);
+
+router.get("/", auth, getDashboards);
+
+router.post("/", createDashboard);
+
+router.put("/:id", updateDashboard);
+
+router.delete("/:id", deleteDashboard);
 
 export default router;
