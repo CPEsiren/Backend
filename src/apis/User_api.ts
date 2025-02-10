bfe1aac74e4f3c3d8feaf02fcca3902f34dc5388
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-10 11:58:07 +0700

    Add getRole endpoint to User API for role retrieval

--- src/apis/User_api.ts
+++ src/apis/User_api.ts
@@ -1,11 +1,18 @@
 import express from "express";
-import { getUser, getUsers, deleteUser } from "../controllers/userController";
+import {
+  getUser,
+  getUsers,
+  deleteUser,
+  getRole,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
 router.get("/:id", getUser);
+
+router.get("/role/:id", getRole);
 
 router.delete("/:id", deleteUser);
 
