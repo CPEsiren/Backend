6b7fa7367acf1f772e6c49bed1acfa57f595d61a
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-12 17:32:15 +0700

    Remove unused getData route from Data_api

--- src/apis/Data_api.ts
+++ src/apis/Data_api.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { getAllData, getBetween, getData } from "../controllers/dataController";
+import { getAllData, getBetween } from "../controllers/dataController";
 
 const router = Router();
 
 router.get("/", getAllData);
 
 router.get("/between", getBetween);
-
-router.get("/:id", getData);
 
 export default router;
