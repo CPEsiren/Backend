4a563dced80964a6bee1597ece63e8ba33dfc8cb
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-22 00:16:56 +0700

    add admin authentication middleware for enhanced access control

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../services/authenService";
+import { User } from "../models/User";
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
@@ -14,6 +15,36 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     await verifyToken(token);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export const authAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Bearer token is required for authentication" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const userInfo = await verifyToken(token);
+
+    const user = await User.findOne({ email: userInfo?.email, role: "admin" });
+
+    if (!user)
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to access this resource" });
     next();
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
