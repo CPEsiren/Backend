9593d89b151a864e75e9193afa9b2cbff8eefcbd
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-22 00:17:38 +0700

    refactor item model to remove unused fields and add isOverview flag

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -8,10 +8,8 @@ export interface IItem extends Document {
   type: "counter" | "integer";
   unit: string;
   interval: number;
-  status: number;
-  createAt: Date;
-  updateAt: Date;
   isBandwidth: boolean;
+  isOverview: boolean;
 }
 
 const itemSchema: Schema<IItem> = new mongoose.Schema(
@@ -26,14 +24,14 @@ const itemSchema: Schema<IItem> = new mongoose.Schema(
     type: { type: String, required: true, enum: ["counter", "integer"] },
     unit: { type: String, required: true },
     interval: { type: Number, default: 10 },
-    status: { type: Number, default: 1 },
     isBandwidth: { type: Boolean, default: false },
+    isOverview: { type: Boolean, default: false },
   },
   {
     timestamps: { createdAt: "createAt", updatedAt: "updateAt" },
   }
 );
 
-// itemSchema.index({ host_id: 1, item_name: 1 }, { unique: true });
+itemSchema.index({ host_id: 1, item_name: 1 }, { unique: true });
 
 export default mongoose.model<IItem>("Item", itemSchema);
