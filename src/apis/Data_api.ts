546178999fdb397ec25249c289f02d276c05a06b
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-22 23:50:16 +0700

    add get data by item_id

--- src/apis/Data_api.ts
+++ src/apis/Data_api.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { getAllData, getBetween } from "../controllers/dataController";
+import {
+  getAllData,
+  getHostBetween,
+  getItemBetween,
+} from "../controllers/dataController";
 import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/", auth, getAllData);
 
-router.get("/between", auth, getBetween);
+router.get("/host/between", auth, getHostBetween);
+
+router.get("/item/between", auth, getItemBetween);
 
 export default router;
--- src/controllers/dataController.ts
+++ src/controllers/dataController.ts
@@ -65,7 +65,7 @@ export const getAllData = async (req: Request, res: Response) => {
   }
 };
 
-export const getBetween = async (req: Request, res: Response) => {
+export const getHostBetween = async (req: Request, res: Response) => {
   const { startTime, endTime, host_id } = req.query;
 
   const requiredFields = ["startTime", "endTime", "host_id"];
@@ -84,6 +84,97 @@ export const getBetween = async (req: Request, res: Response) => {
       {
         $match: {
           "metadata.host_id": new mongoose.Types.ObjectId(host_id as string),
+          timestamp: {
+            $gte: new Date(startTime as string),
+            $lte: new Date(endTime as string),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            host_id: "$metadata.host_id",
+            item_id: "$metadata.item_id",
+            isBandwidth: "$metadata.isBandwidth",
+          },
+          data: {
+            $push: {
+              timestamp: "$timestamp",
+              value: "$value",
+            },
+          },
+          max_value: { $max: "$value" },
+          min_value: { $min: "$value" },
+          avg_value: { $avg: "$value" },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id.host_id",
+          items: {
+            $push: {
+              item_id: "$_id.item_id",
+              isBandwidth: "$_id.isBandwidth",
+              max_value: "$max_value",
+              min_value: "$min_value",
+              avg_value: "$avg_value",
+              data: "$data",
+            },
+          },
+        },
+      },
+    ]);
+
+    await Data.populate(data, [
+      {
+        path: "items.item_id",
+        model: "Item",
+        select: "_id item_name oid type unit",
+      },
+      {
+        path: "_id",
+        model: "Host",
+        select: "hostname",
+      },
+    ]);
+
+    if (!data.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "No data found between the specified times.",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Data fetched successfully for the specified time range.",
+      data: data,
+    });
+  } catch (error) {
+    console.error("Error fetching data between times:", error);
+    res.status(500).json({ status: "fail", message: "Internal server error" });
+  }
+};
+
+export const getItemBetween = async (req: Request, res: Response) => {
+  const { startTime, endTime, item_id } = req.query;
+
+  const requiredFields = ["startTime", "endTime", "item_id"];
+  const missingFields = requiredFields.filter((field) => !req.query[field]);
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing required fields.",
+      requiredFields: missingFields,
+    });
+  }
+
+  try {
+    const data = await Data.aggregate([
+      {
+        $match: {
+          "metadata.item_id": new mongoose.Types.ObjectId(item_id as string),
           timestamp: {
             $gte: new Date(startTime as string),
             $lte: new Date(endTime as string),
