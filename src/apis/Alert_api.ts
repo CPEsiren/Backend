1d0e89b59dd7dbc18e2b11cf00b777df9485c5b1
Smart309 <pattharapon_takham@cmu.ac.th>
2025-03-03 20:51:55 +0700

    refactor: remove Alert API and associated controller and model files

--- src/apis/Alert_api.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllAlerts } from "../controllers/alertController";
-
-const router = Router();
-
-router.get("/", getAllAlerts);
-
-export default router;
